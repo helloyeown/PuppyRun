f57ca325747628e0222eb29b01d0b4109aa837c6
helloyeown <toquasimodox@gmail.com>
2024-03-15 09:41:24 +0900

    jump

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+var jumpTimer = 0;
 
 canvas.width = window.innerWidth - 100;
 canvas.height = window.innerHeight - 100;
@@ -52,12 +53,35 @@ function eachFrame() {
         huddles.push(huddle);
     }
     
-    huddles.forEach(function(a) {
+    huddles.forEach(function(a, i, o) {
+        // x 좌표가 0 미만이면 제거 (배열에 계속 쌓이는 장애물을 제거)
+        if (a.x < 0) {
+            o.splice(i, 1);
+        }
+
         a.x -= 5;       // 장애물 다가오는 속도
         a.draw();
     });
 
+    // 점프
+    if (jumping == true) {
+        puppy.y--;
+        jumpTimer++;    // 프레임마다 +1
+    }
+
+    if (jumpTimer > 100) {     // 100frame 넘으면 jump 중단
+        jumping = false;
+    }
+
     puppy.draw();
 }
 
+// 프레임마다 실행
 eachFrame();
+
+var jumping = false;    // 점프 중
+document.addEventListener('keydown', function(e) {
+    if (e.code === 'Space') {
+        jumping = true;
+    }
+})
